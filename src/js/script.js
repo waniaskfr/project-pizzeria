1e7f9be94dbf28891ab08d823882d9b01717a83d
Łukasz Kania <waniaskfr@gmail.com>
2019-10-31 15:21:00 +0100

    Add instance for each product

--- src/js/script.js
+++ src/js/script.js
@@ -52,7 +52,31 @@
     menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
   };
 
+  class Product {
+    constructor(id, data){
+      const thisProduct = this;
+      thisProduct.id = id;
+      thisProduct.data = data;
+      console.log('new Product:', thisProduct);
+    }
+  }
+
   const app = {
+    initMenu: function(){
+      const thisApp = this;
+      console.log('thisApp.data:', thisApp.data);
+
+      for(let productData in thisApp.data.products){
+        new Product(productData, thisApp.data.products[productData]);
+      }
+    },
+
+    initData: function(){
+      const thisApp = this;
+
+      thisApp.data = dataSource;
+    },
+
     init: function(){
       const thisApp = this;
       console.log('*** App starting ***');
@@ -60,8 +84,12 @@
       console.log('classNames:', classNames);
       console.log('settings:', settings);
       console.log('templates:', templates);
+      
+      thisApp.initData();  
+      thisApp.initMenu();
     },
   };
-
+  
+  
   app.init();
 }
