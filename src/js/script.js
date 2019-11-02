ac6477352e3d3148a8b602097b7f3052495c77b3
Łukasz Kania <waniaskfr@gmail.com>
2019-11-02 17:20:55 +0100

    Add initAccordion

--- src/js/script.js
+++ src/js/script.js
@@ -60,6 +60,7 @@
       thisProduct.data = data;
 
       thisProduct.renderInMenu();
+      thisProduct.initAccordion(); 
 
       console.log('new Product:', thisProduct);
     }
@@ -74,6 +75,35 @@
       const menuContainer = document.querySelector(select.containerOf.menu);
       /* add element to menu */
       menuContainer.appendChild(thisProduct.element);
+    }
+
+    initAccordion(){
+      const thisProduct = this;
+      /* find the clickable trigger (element that should react to clicking) */
+      const clickableTrigger = document.querySelector(select.menuProduct.clickable);
+      console.log('clickableTrigger:', clickableTrigger); 
+      /* START: click event listener to trigger */
+      clickableTrigger.addEventListener('click', function(){
+      console.log('clicked');
+        /* prevent default action for event */
+        event.preventDefault();
+        /* toggle active class on element of thisProduct */
+        thisProduct.element.classList.toggle('active');
+        /* find all active products */
+        const activeProducts = document.querySelectorAll(select.menuProductsActive);
+        /* START LOOP: for each active product */
+          for(let activeProduct of activeProducts){
+          /* START: if the active product isn't element of thisProduct */
+           if(activeProduct !== thisProduct){
+            /* remove class active for the active product */
+           activeProduct.classList.remove('active');
+          /* END: if the active product isn't the element of thisProduct */
+          }
+        /* END LOOP: for each active product */
+      }
+      /* END: click event listener to trigger */
+      });
+
     }
   }
 
