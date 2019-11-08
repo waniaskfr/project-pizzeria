091b821af48bdf2ce87926c307872448c341f6de
Łukasz Kania <waniaskfr@gmail.com>
2019-11-08 13:09:39 +0100

    Add validation

--- src/js/script.js
+++ src/js/script.js
@@ -220,6 +220,7 @@
       const thisWidget = this;
 
       thisWidget.getElements(element);
+      thisWidget.value = settings.amountWidget.defaultValue;
       thisWidget.setValue(thisWidget.input.value);
       thisWidget.initActions();
 
@@ -244,9 +245,14 @@
       const newValue = parseInt(value);
 
       /* TODO: Add validation */
+      if(newValue != thisWidget.value 
+        && newValue >= settings.amountWidget.defaultMin 
+        && newValue <= settings.amountWidget.defaultMax)
+        
+        thisWidget.value = newValue;
+        thisWidget.announce();
+      
 
-      thisWidget.value = newValue;
-      thisWidget.announce();
       thisWidget.input.value = thisWidget.value;
     }
     
