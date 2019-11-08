602ad5e1808f2ad014bca83bfca461ec06822c96
Łukasz Kania <waniaskfr@gmail.com>
2019-11-08 10:37:42 +0100

    Add listeners to InitActions

--- src/js/script.js
+++ src/js/script.js
@@ -214,10 +214,12 @@
       const thisWidget = this;
 
       thisWidget.getElements(element);
+      thisWidget.setValue(thisWidget.input.value);
 
       console.log('AmountWidget:', thisWidget);
       console.log('constructor arguments:', element);
     }
+    
     getElements(element){
       const thisWidget = this;
 
@@ -226,12 +228,41 @@
       thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
       thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
     }
+    
+    setValue(value){
+      const thisWidget = this;
+
+      const newValue = parseInt(value);
+
+      /* TODO: Add validation */
+
+      thisWidget.value = newValue;
+      thisWidget.input.value = thisWidget.value;
+    }
+    
+    initActions(){
+      const thisWidget = this;
+
+      thisWidget.input.addEventListener('change', function(){
+        thisWidget.setValue(thisWidget.input.value);
+      });
+
+      thisWidget.linkDecrease.addEventListener('click', function(event){
+        event.preventDefault();
+        thisWidget.setValue(thisWidget.value - 1);
+      });
+
+      thisWidget.linkIncrease.addEventListener('click', function(event){
+        event.preventDefault();
+        thisWidget.setValue(thisWidget.value + 1);
+      });
+    }
   }
 
   const app = {
     initMenu: function(){
       const thisApp = this;
-       console.log('thisApp.data:', thisApp.data);
+      console.log('thisApp.data:', thisApp.data);
 
       for(let productData in thisApp.data.products){
         new Product(productData, thisApp.data.products[productData]);
@@ -246,11 +277,11 @@
 
     init: function(){
       const thisApp = this;
-       console.log('*** App starting ***');
-       console.log('thisApp:', thisApp);
-       console.log('classNames:', classNames);
-       console.log('settings:', settings);
-       console.log('templates:', templates);
+      console.log('*** App starting ***');
+      console.log('thisApp:', thisApp);
+      console.log('classNames:', classNames);
+      console.log('settings:', settings);
+      console.log('templates:', templates);
       
       thisApp.initData();  
       thisApp.initMenu();
